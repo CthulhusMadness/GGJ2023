d3e1a4f135ff1193c35282feb37097c86247b9f3
CthulhusMandess <mikysanto.97@gmail.com>
2023-02-04 16:25:09 +0100

    Added PlayerInteraction clas

---
+++ Take Roots/Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("House"))
+            Debug.Log("House touched");
+    }
+}
