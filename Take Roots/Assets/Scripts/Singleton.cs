913f60c3b028b2158241141fe249d660767a33fe
CthulhusMandess <mikysanto.97@gmail.com>
2023-02-04 23:39:45 +0100

    Added Singleton class

---
+++ Take Roots/Assets/Scripts/Singleton.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Instance { get; private set; }
+
+    private void Awake()
+    {
+        // If there is an instance, and it's not me, delete myself.
+        if (Instance != null && Instance != this)
+            Destroy(this);
+        else
+        {
+            Instance = this as T;
+            DontDestroyOnLoad(this);
+        }
+    }
+}
