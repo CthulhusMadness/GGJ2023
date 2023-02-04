fd3a7fc20725475b36df5c04aeb8b0e3cdf41d24
CthulhusMandess <mikysanto.97@gmail.com>
2023-02-05 00:24:04 +0100

    Changed class name to CameraManager

--- Take Roots/Assets/Scripts/CameraManager.cs
+++ Take Roots/Assets/Scripts/CameraManager.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowPlayer : MonoBehaviour
+public class CameraManager : MonoBehaviour
 {
-
     public Transform target;
     public float speed;
 
     // Update is called once per frame
     private void FixedUpdate()
     {
+        FollowTarget();
+    }
+
+    private void FollowTarget()
+    {
         Vector3 movementDir = new Vector3(transform.position.x - target.position.x, 0, 0);
-        if(Mathf.Abs(transform.position.x - target.position.x) > 0)
+        if (Mathf.Abs(transform.position.x - target.position.x) > 0)
         {
             transform.Translate(-Vector3.right * speed * movementDir.x, Space.World);
         }
