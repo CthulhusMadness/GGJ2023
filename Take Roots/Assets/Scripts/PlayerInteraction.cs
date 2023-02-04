207b70c9dbf012ea0f8cb0570e76e0a6079cefa7
CthulhusMandess <mikysanto.97@gmail.com>
2023-02-04 18:26:23 +0100

    Added OnTriggerExit() method; Added meta file

--- Take Roots/Assets/Scripts/PlayerInteraction.cs
+++ Take Roots/Assets/Scripts/PlayerInteraction.cs
@@ -7,6 +7,18 @@ public class PlayerInteraction : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("House"))
+        {
+            collision.GetComponentInParent<House>().SetActiveInteractButton(true);
             Debug.Log("House touched");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("House"))
+        {
+            collision.GetComponentInParent<House>().SetActiveInteractButton(false);
+            Debug.Log("House exit");
+        }
     }
 }
