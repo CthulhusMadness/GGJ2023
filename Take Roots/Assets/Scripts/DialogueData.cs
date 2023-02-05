2e9652d00b26142dcab11a8ae8a918c57c38d31e
CthulhusMandess <mikysanto.97@gmail.com>
2023-02-05 16:02:06 +0100

    Added typingSpeed and thirdChoise fields

--- Take Roots/Assets/Scripts/DialogueData.cs
+++ Take Roots/Assets/Scripts/DialogueData.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Dialogue", menuName = "Dialogue")]
 public class DialogueData : ScriptableObject
 {
+    public float typingSpeed = .1f;
+    [Multiline(10)]
     public string text;
     public string firstChoice;
     public string secondChoice;
-
+    public string thirdChoice;
 }
