2525ab36ebdb8fb2cf934262a5fb219b6262199e
CthulhusMandess <mikysanto.97@gmail.com>
2023-02-05 20:18:39 +0100

    Added scores

--- Take Roots/Assets/Scripts/GameManager.cs
+++ Take Roots/Assets/Scripts/GameManager.cs
@@ -5,4 +5,44 @@ using UnityEngine;
 public class GameManager : PersistentSingleton<GameManager>
 {
     public int Day = 1;
+    [SerializeField] private int birdScore;
+    [SerializeField] private int dogScore;
+    [SerializeField] private int cowScore;
+
+    public void ResetScore()
+    {
+        birdScore = 0;
+        dogScore = 0;
+        cowScore = 0;
+    }
+
+    public void setBirdScore(int score)
+    {
+        birdScore += score;
+    }
+
+    public void setDogScore(int score)
+    {
+        dogScore += score;
+    }
+
+    public void setCowScore(int score)
+    {
+        cowScore += score;
+    }
+
+    public int getBirdScore()
+    {
+        return birdScore;
+    }
+
+    public int getDogScore()
+    {
+        return dogScore;
+    }
+
+    public int getCowScore()
+    {
+        return cowScore;
+    }
 }
