a8b3b301a0fcf6d32b71f1934d6b6d010cdbaafb
CthulhusMandess <mikysanto.97@gmail.com>
2023-02-05 19:56:02 +0100

    Added sceneManager

---
+++ Take Roots/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnitySceneManager = UnityEngine.SceneManagement.SceneManager;
+
+public class SceneManager : Singleton<SceneManager>
+{
+    [SerializeField] private int day;
+    [SerializeField] private string nextScene;
+    [SerializeField, Min(0)] private float changeSceneDelay;
+
+    public event Action<bool> OnStartDay;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        GameManager.Instance.Day = day;
+    }
+
+    private void Start()
+    {
+        OnStartDay?.Invoke(true);
+    }
+
+    public void ReturnMenu()
+    {
+        LoadScene("Menu");
+    }
+
+    public void ChangeScene()
+    {
+        StartCoroutine(ChangeSceneDelay());
+    }
+
+    private IEnumerator ChangeSceneDelay()
+    {
+        yield return new WaitForSeconds(changeSceneDelay);
+        LoadScene(nextScene);
+    }
+
+    private void LoadScene(string scene)
+    {
+        UnitySceneManager.LoadScene(scene);
+    }
+}
