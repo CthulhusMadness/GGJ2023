b61b4ef64649d565f221374996dad5ae06eccc46
CthulhusMandess <mikysanto.97@gmail.com>
2023-02-05 01:02:20 +0100

    Added zoooming logic

--- Take Roots/Assets/Scripts/CameraManager.cs
+++ Take Roots/Assets/Scripts/CameraManager.cs
@@ -1,11 +1,20 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    public Transform target;
-    public float speed;
+    [SerializeField] private Camera cam;
+    [SerializeField] private Transform target;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float zoomSpeedMultiplier;
+    [SerializeField] private AnimationCurve zoomCurve;
+
+    private Coroutine coroutine;
+
+    private void Start()
+    {
+        cam.orthographicSize = 5f;
+    }
 
     // Update is called once per frame
     private void FixedUpdate()
@@ -17,8 +26,30 @@ public class CameraManager : MonoBehaviour
     {
         Vector3 movementDir = new Vector3(transform.position.x - target.position.x, 0, 0);
         if (Mathf.Abs(transform.position.x - target.position.x) > 0)
+            transform.Translate(-Vector3.right * moveSpeed * movementDir.x, Space.World);
+    }
+
+    public void Zoom(bool zoomIn)
+    {
+        if (coroutine != null)
+            StopCoroutine(coroutine);
+        coroutine = StartCoroutine(ZoomDelay(zoomIn));
+    }
+
+    private IEnumerator ZoomDelay(bool zoomIn)
+    {
+        float sizeTarget = zoomIn ? 3f : 5f;
+
+        float timer = 0f;
+        float curveValue = 0f;
+        while (curveValue < 1)
         {
-            transform.Translate(-Vector3.right * speed * movementDir.x, Space.World);
+            timer += Time.fixedDeltaTime * zoomSpeedMultiplier;
+            curveValue = zoomCurve.Evaluate(timer);
+            Debug.Log(curveValue);
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, sizeTarget, curveValue);
+            yield return null;
         }
+        cam.orthographicSize = sizeTarget;
     }
 }
