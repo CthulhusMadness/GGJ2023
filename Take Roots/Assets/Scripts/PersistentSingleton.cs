a6d67fa820029c5fc2c1ca6be90a1657c4087c82
CthulhusMandess <mikysanto.97@gmail.com>
2023-02-05 19:55:52 +0100

    Added PersistentSingleton class

---
+++ Take Roots/Assets/Scripts/PersistentSingleton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public abstract class PersistentSingleton<T> : Singleton<T> where T : MonoBehaviour
+{
+    protected override void Awake()
+    {
+        // If there is an instance, and it's not me, delete myself.
+        if (Instance != null && Instance != this)
+            Destroy(this);
+        else
+        {
+            Instance = this as T;
+            DontDestroyOnLoad(this);
+        }
+    }
+}
--- Take Roots/Assets/Scripts/Singleton.cs
+++ Take Roots/Assets/Scripts/Singleton.cs
@@ -1,20 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
-    public static T Instance { get; private set; }
+    public static T Instance { get; protected set; }
 
-    private void Awake()
+    protected virtual void Awake()
     {
         // If there is an instance, and it's not me, delete myself.
         if (Instance != null && Instance != this)
             Destroy(this);
         else
-        {
             Instance = this as T;
-            DontDestroyOnLoad(this);
-        }
     }
 }
